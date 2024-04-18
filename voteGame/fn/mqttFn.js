4c3b466ffc5440537ce63226e624287f77a2a0f1
doublethlee <mark2lee@hotmail.com>
2024-04-18 09:46:04 +0800

    Update mqttFn.js

--- voteGame/fn/mqttFn.js
+++ voteGame/fn/mqttFn.js
@@ -6,9 +6,9 @@
  * @Description: file content
  */
 var host = 'broker.emqx.io' // 环信MQTT服务器地址 通过console后台[MQTT]->[服务概览]->[服务配置]下[连接地址]获取
-var port = 8083 // 协议服务端口 1883/1884/80/443 通过console后台[MQTT]->[服务概览]->[服务配置]下[连接端口]获取
+var port = 8084 // 协议服务端口 1883/1884/80/443 通过console后台[MQTT]->[服务概览]->[服务配置]下[连接端口]获取
 
-var useTLS = false // 是否走加密 HTTPS，如果走 HTTPS，设置为 true
+var useTLS = true // 是否走加密 HTTPS，如果走 HTTPS，设置为 true
 var cleansession = false;
 var clientId = 'emqx' + Math.random().toString(16).substr(2, 8) // 客户端ID，随机生成即可
 var reconnectTimeout = 2000 // 超时重连时间
